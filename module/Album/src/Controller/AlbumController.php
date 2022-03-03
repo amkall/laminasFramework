f11948b5a236d7e850ee78e30911843bf7763c71
amkall <brunogt03826@gmail.com>
2022-03-03 17:00:26 -0300

    add action

--- module/Album/src/Controller/AlbumController.php
+++ module/Album/src/Controller/AlbumController.php
@@ -6,6 +6,13 @@ use Album\Model\AlbumTable;
 use Album\Model\CantorTable;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
+use Laminas\I18n\View\Helper\AbstractTranslatorHelper;
+use Laminas\I18n\Translator\TranslatorAwareInterface;
+
+
+// Add the following import statements at the top of the file:
+use Album\Form\AlbumForm;
+use Album\Model\Album;
 
 class AlbumController extends AbstractActionController
 {
@@ -28,9 +35,30 @@ class AlbumController extends AbstractActionController
         ]);
     }
 
-    public function addAction()
-    {
-    }
+   /* Update the following method to read as follows: */
+   public function addAction()
+   {
+       $form = new AlbumForm();
+       $form->get('submit')->setValue('Add');
+
+       $request = $this->getRequest();
+
+       if (! $request->isPost()) {
+           return ['form' => $form];
+       }
+
+       $album = new Album();
+       $form->setInputFilter($album->getInputFilter());
+       $form->setData($request->getPost());
+
+       if (! $form->isValid()) {
+           return ['form' => $form];
+       }
+
+       $album->exchangeArray($form->getData());
+       $this->table->saveAlbum($album);
+       return $this->redirect()->toRoute('album');
+   }
 
     public function editAction()
     {
